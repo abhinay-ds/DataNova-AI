698761b1d3090cfb7737ad97c70ae19e3bff3161
abhinay-ds <abhinaypolisetty0@gmail.com>
2026-07-15 14:24:12 +0530

    Build professional navbar

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,7 +1,49 @@
+import {
+  Bell,
+  Moon,
+  ChevronDown,
+  CircleUserRound,
+} from "lucide-react";
+
 export default function Navbar() {
   return (
-    <div className="h-16 bg-white border-b flex items-center px-6">
-      Navbar
-    </div>
+    <header className="h-16 bg-white border-b border-slate-200 px-8 flex items-center justify-between">
+
+      {/* Workspace */}
+      <div className="flex items-center gap-2">
+
+        <h2 className="font-semibold text-lg">
+          Workspace
+        </h2>
+
+        <ChevronDown size={18} />
+
+      </div>
+
+      {/* Right Side */}
+
+      <div className="flex items-center gap-6">
+
+        <button className="hover:text-blue-600 transition">
+          <Moon size={20} />
+        </button>
+
+        <button className="hover:text-blue-600 transition">
+          <Bell size={20} />
+        </button>
+
+        <div className="flex items-center gap-2">
+
+          <CircleUserRound size={30} />
+
+          <span className="font-medium">
+            Guest
+          </span>
+
+        </div>
+
+      </div>
+
+    </header>
   );
 }
